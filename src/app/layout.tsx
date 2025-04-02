98f175726f215dbacf795a272c5390a1e6508baa
Gustavo Weber <weber1.caxias@gmail.com>
2025-04-02 10:30:46 -0300

    att favicon and metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,7 +10,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Trackbit",
+  title: "Grupo Serra Online",
   description: "Controle seu tempo de forma intuitiva e melhore sua produtividade.",
 };
 
